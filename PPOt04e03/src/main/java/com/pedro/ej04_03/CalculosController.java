497400afd56db9d316ccbb50c1fc353641f2e9bc
PedroPineiro <ppineiroordax@gmail.com>
2025-10-18 17:43:57 +0200

    ej3 completo

--- PPOt04e03/src/main/java/com/pedro/ej04_03/CalculosController.java
+++ PPOt04e03/src/main/java/com/pedro/ej04_03/CalculosController.java
@@ -25,26 +25,59 @@ public class CalculosController {
     }
 
     @GetMapping("/primo")
-    public String esPrimo(@RequestParam int numero, Model model) {
-        boolean resultadoEsPrimo = calculosService.esPrimo(numero);
-        model.addAttribute("resultadoEsPrimo", resultadoEsPrimo);
-        model.addAttribute("numero", numero);
-        return "resultadoPrimo";
+    public String esPrimo(@RequestParam(required=false) Integer numero, Model model) {
+        try {
+            if (numero == null) {
+                throw new IllegalArgumentException("Se tiene que mandar un parámetro");
+            }
+            else if(numero < 0) {
+                throw new IllegalArgumentException("El número no puede ser negativo.");
+            }
+            else{
+                boolean resultadoEsPrimo = calculosService.esPrimo(numero);
+                model.addAttribute("resultadoEsPrimo", resultadoEsPrimo);
+                model.addAttribute("numero", numero);
+                return "resultadoPrimo";
+            }
+            
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("mensajeError", e.getMessage());
+            return "errorView";
+        } 
     }
     
     
     @GetMapping("/hipotenusa/{x}/{y}")
     public String hipotenusa(@PathVariable int x, @PathVariable int y, Model model) {
-        double resultadoHipotenusa = calculosService.calcularHipotenusa(x, y);
-        model.addAttribute("resultadoHipotenusa",resultadoHipotenusa);
-        return "resultadoHipotenusa";
+        try {
+            if(x > 0 && y > 0){
+                double resultadoHipotenusa = calculosService.calcularHipotenusa(x, y);
+                model.addAttribute("resultadoHipotenusa",resultadoHipotenusa);
+                return "resultadoHipotenusa";
+            }
+            else{
+                throw new IllegalArgumentException("Los catetos deben mayores que 0.");
+            }
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("mensajeError", e.getMessage());
+            return "errorView";
+        }
     }
 
     @GetMapping("/divisores/{x}")
     public String divisores(@PathVariable int x, Model model) {
-        calculosService.limpiar();
-        ArrayList<Integer> listaDivisores = calculosService.calcularDivisores(x);
-        model.addAttribute("listaDivisores", listaDivisores);
-        return "resultadoDivisores";
+        try {
+            if(x > 0){
+                calculosService.limpiar();
+                ArrayList<Integer> listaDivisores = calculosService.calcularDivisores(x);
+                model.addAttribute("listaDivisores", listaDivisores);
+                return "resultadoDivisores";
+            } else{
+                throw new IllegalArgumentException("El número debe ser mayor que 0.");
+            }
+        } catch (IllegalArgumentException e) {
+            model.addAttribute("mensajeError", e.getMessage());
+            return "errorView";
+        }
     }
 }
